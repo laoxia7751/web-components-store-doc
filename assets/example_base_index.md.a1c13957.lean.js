3747d357bd34cc64807acb1b47c709c4faf8428c
xiajitao <xiajitao@genew.com>
2023-04-04 08:36:24 +0800

    'fix:验证路径问题'

---
+++ assets/example_base_index.md.a1c13957.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as l,o as a,O as n}from"./chunks/framework.dc586db8.js";const A=JSON.parse('{"title":"说明","description":"","frontmatter":{},"headers":[],"relativePath":"example/base/index.md"}'),p={name:"example/base/index.md"},o=n("",5),e=[o];function t(D,c,F,r,y,C){return a(),l("div",null,e)}const b=s(p,[["render",t]]);export{A as __pageData,b as default};
